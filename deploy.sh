827a0c77267678726f9ed95376903345086952e9
fangyan <fangyan@qunhemail.com>
2023-06-11 08:11:26 +0800

    Deploy to GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ npm run build && cd./build
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quite "git@github.com:pengfeiw/three-tester.git" master:gh-pages > /dev/null 2>&1
-rm -rf build
+git push --force "git@github.com:pengfeiw/three-tester.git" master:gh-pages > /dev/null 2>&1
+# rm -rf build
 echo "Deploy to GitHub Pages - Success"
