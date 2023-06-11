a95115b4005850bd53094b533f8849d75c461f81
fangyan <fangyan@qunhemail.com>
2023-06-11 08:03:57 +0800

    Deploy to GitHub Pages

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+echo "Start build project."
+npm run build && cd./build
+git init
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force "git@github.com:pengfeiw/three-tester.git" master:gh-pages > /dev/null 2>&1
+
+echo "Start clear build files."
+rm -rf build
+
+echo "Deploy to GitHub Pages - Success"
