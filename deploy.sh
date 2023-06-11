facdc97c1774c4e7fa4a26cf6a9155addcc7f7e3
fangyan <fangyan@qunhemail.com>
2023-06-11 08:24:19 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 echo "Start build project."
-npm run build && cd./build
+npm run build
+cd ./build
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
-git remote add origin git@github.com:pengfeiw/three-tester.git
-git push -u --force origin gh-pahges
-# git push --force "git@github.com:pengfeiw/three-tester.git" master:gh-pages
-# rm -rf build
+git push --force "git@github.com:pengfeiw/three-tester.git" master:gh-pages
+rm -rf ./build
 echo "Deploy to GitHub Pages - Success"
