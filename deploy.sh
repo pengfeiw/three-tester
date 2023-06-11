36fa2122f3c4c18cdb26b8d237d61ad0f7d30661
fangyan <fangyan@qunhemail.com>
2023-06-11 08:18:14 +0800

    Deploy to GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ npm run build && cd./build
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force "git@github.com:pengfeiw/three-tester.git" master:gh-pages
+git remote add origin git@github.com:pengfeiw/three-tester.git
+git push -u --force origin gh-pahges
+# git push --force "git@github.com:pengfeiw/three-tester.git" master:gh-pages
 # rm -rf build
 echo "Deploy to GitHub Pages - Success"
